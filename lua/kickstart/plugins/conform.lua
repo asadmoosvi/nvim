48cf998157ed1188c78515b1206d10c84ebfe795
Asad Moosvi <moosvi.asad@gmail.com>
2024-08-27 21:31:43 +0500

    [conform] set prettier as the html formatter

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -32,6 +32,7 @@ return {
                 yaml = { 'yamlfmt' },
                 json = { 'prettier' },
                 sh = { 'shfmt' },
+                html = { 'prettier' },
                 -- You can use a sub-list to tell conform to run *until* a formatter
                 -- is found.
                 -- javascript = { { "prettierd", "prettier" } },
