507c99d251118cea95d296d0bbfd2091720ac1b6
Asad Moosvi <moosvi.asad@gmail.com>
2024-08-29 19:18:13 +0500

    Add typescript language server

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -181,6 +181,7 @@ return {
                 },
                 emmet_language_server = {},
                 cssls = {},
+                tsserver = {},
             }
 
             -- Ensure the servers and tools above are installed
