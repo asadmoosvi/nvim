cd93f165eac0885c65ea69c4f63b054bff205208
Asad Moosvi <moosvi.asad@gmail.com>
2024-09-09 15:33:18 +0500

    [lsp] add sql language server `sqls`
    Disable the lsp formatting for this language server as there's a bug
    currently that removes all whitespaces. See https://github.com/sqls-server/sqls/issues/149

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -18,7 +18,7 @@ return {
                 -- Disable "format_on_save lsp_fallback" for languages that don't
                 -- have a well standardized coding style. You can add additional
                 -- languages here or re-enable it for the disabled ones.
-                local disable_filetypes = { c = false, cpp = false }
+                local disable_filetypes = { c = false, cpp = false, sql = true }
                 return {
                     timeout_ms = 500,
                     lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
