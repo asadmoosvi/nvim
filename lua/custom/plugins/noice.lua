1bf9bb3c723a36d09851c61a2f23eff31df6ecac
Asad Moosvi <moosvi.asad@gmail.com>
2025-01-04 08:56:35 +0500

    [noice] use mini view for all messages
    The default notify view is a bit distracting.

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -30,15 +30,8 @@ return {
                 inc_rename = false, -- enables an input dialog for inc-rename.nvim
                 lsp_doc_border = false, -- add a border to hover docs and signature help
             },
-            routes = {
-                {
-                    view = 'mini',
-                    filter = {
-                        event = 'msg_show',
-                        kind = '',
-                        find = 'written',
-                    },
-                },
+            messages = {
+                view = 'mini',
             },
         }
     end,
